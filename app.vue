95e113871f566f21996f0ed5e86fba355f3b75f3
Tibor Kovacs <tibor.kovacs@onelogin.com>
2023-12-19 09:33:18 +0100

    wip

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script>
+import FrameWorks from "./pages/frame-works.vue";
+export default {
+  components: {FrameWorks}
+}
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  const {data: nuxt_repo} = await useFetch('https://api.github.com/repos/nuxt/nuxt');
+  const {data: angular_repo} = await
+    useFetch('https://api.github.com/repos/angular/angular');
+</script>
+
+<template>
+  <div>
+    Nuxt stars: <span>{{nuxt_repo?.stargazers_count}}</span>
+  </div>
+  <div>
+    Angular stars: <span>{{angular_repo.stargazers_count}}</span>
+  </div>
+</template>
